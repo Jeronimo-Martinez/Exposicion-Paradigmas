1ac292f4eb176d99e14933ce18204c3424b96699
Silvana Saavedra Londoño <silvana.saavedra@udea.edu.co>
2025-09-18 13:36:59 -0500

    Creación de lista de clientes
    En este commit trabajaron en pair programming Valentina Flórez y Silvana Saavedra

--- Banco.hs
+++ Banco.hs
@@ -1,6 +1,14 @@
-
+-- Se define el tipo de dato cliente 
 data Cliente = Cliente
   { idCliente :: Int       -- Identificador
   , cuenta    :: String    -- Número de cuenta
   , saldo     :: Float     -- Saldo actual
   } deriving (Show, Eq)
+
+--Se crea una lista inicial de clientes para probar las funciones 
+clientesIniciales :: [Cliente]
+clientesIniciales = [
+    Cliente 1 "Ana" 1000.0,
+    Cliente 2 "Luis" 500.0,
+    Cliente 3 "Maria" 200.0
+    ]
